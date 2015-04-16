42f1821aa61169daf1532a4877fe05ee56c8fec7
Greg Woods <greg@westlakedesign.com>
2015-04-16 12:49:17 -0400

    move get_or_auto_create_assoc to a private method

--- lib/automatic_record/auto_create.rb
+++ lib/automatic_record/auto_create.rb
@@ -6,18 +6,6 @@ module AutomaticRecord
 
     end
 
-    def get_or_auto_create_assoc(assoc, force_reload=false, default_attrs_or_block={}) # :nodoc:
-      result = method(assoc).super_method.call(force_reload)
-      if result.blank?
-        if default_attrs_or_block.is_a?(Proc)
-          result = default_attrs_or_block.call(self)
-        else
-          result = send("create_#{assoc}", default_attrs_or_block)
-        end
-      end
-      return result
-    end
-
     module ClassMethods
 
       # Specifies that the given association should be created automaticaly whenever it is accessed, if
@@ -51,6 +39,20 @@ module AutomaticRecord
           end
         end
       end
+    end
+
+private
+
+    def get_or_auto_create_assoc(assoc, force_reload=false, default_attrs_or_block={}) # :nodoc:
+      result = method(assoc).super_method.call(force_reload)
+      if result.blank?
+        if default_attrs_or_block.is_a?(Proc)
+          result = default_attrs_or_block.call(self)
+        else
+          result = send("create_#{assoc}", default_attrs_or_block)
+        end
+      end
+      return result
     end
 
   end
